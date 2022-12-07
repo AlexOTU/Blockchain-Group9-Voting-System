275ef974d8a06ba80a7281800092d73abcb0ada4
Haider030-DASH <71289272+Haider030-DASH@users.noreply.github.com>
2022-12-06 23:30:01 -0500

    Update Final.py

--- Final.py
+++ Final.py
@@ -46,3 +46,43 @@ class Block: # defining the Block class
                 temp = json.load(f)
                 if str(temp['id']) == str(self.parentID):
                     return temp
+                
+def load(blockchain):
+    # if the block directory is empty, call the generateGenesis function and recall the function
+    if os.listdir(BLOCK_DIR) == []:
+        generateGenesis()
+        load(blockchain)
+    else:
+        # open each block in the directory, load the attributes into a new Block object and append it to the blockchain list
+        for block in os.listdir(BLOCK_DIR):
+            with open(f'{BLOCK_DIR}\\{block}', 'r', encoding='utf-8') as f:
+                content = json.load(f)
+                blockchain.append(Block(
+                    content['id'],
+                    content['timestamp'],
+                    content['parentID'],
+                    content['content'],
+                    content['PoW'],
+                    content['hash'],
+                    ))
+        order(blockchain)
+# sort the blocks by timestamp
+def order(blockchain):
+    return blockchain.sort(key=lambda x: x.timestamp, reverse=True)
+# generates the genesis block
+def generateGenesis():
+    print('Genesis block not detected. Creating Genesis.')
+    Block(id="0", timestamp = int(datetime.datetime.now().timestamp()), parentID="0", content=str(input("ENTER POLL QUESTION > ")), PoW=DIFFICULTY, hash="0").writeBlockToJSON()
+
+def generateHash(parent, PoW):
+    count = 0
+    # read the ID of the latest block
+    with open(f'{BLOCK_DIR}\\{parent.id}.json', 'rb') as f:
+        while True:
+            # hash the ID and 64 random string bytes
+            hash = hashlib.pbkdf2_hmac('sha256', str(f.read()).encode(), str(os.urandom(64)).encode(), 1, dklen=128).hex()
+            count += 1
+            # checking if the number of 0s in the hash is equal to the PoW, then return the hash
+            if str('0' * PoW) == str(hash)[0: PoW]:
+                print(f'\r{count} Hashes Checked. Difficulty set to {PoW}.\n')
+                return hash
